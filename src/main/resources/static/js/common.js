648901a7a57fcfbc401a22f5143f5bb147e911f7
Wisniewski <wisniewk@emea.roche.com>
2017-12-11 18:04:50 +0100

    Add link validation

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,9 +1,15 @@
 $(function() {
 	$( document ).tooltip();
+	$('#view').addClass("disabled");
+	$('#edit').addClass("disabled");
+	$('#delete').addClass("disabled");
 });
 
 function setSelected(radio) {
 	$('#selected').attr('value', radio.value);
+	$('#view').removeClass("disabled");
+	$('#edit').removeClass("disabled");
+	$('#delete').removeClass("disabled");
 }
 
 function sendAction(actionName) {
